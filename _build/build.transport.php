59c7acd6c6869d8c97848a6f3640442f2dd950fd
bobray <bobray@softville.com>
2011-11-28 21:32:02 -0500

    Added build

---
+++ _build/build.transport.php
@@ -0,0 +1,208 @@
+<?php
+/**
+ * ObjectExplorer Build Script
+ *
+ *
+ * ObjectExplorer is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * ObjectExplorer is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ObjectExplorer; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package objectexplorer
+ * @subpackage build
+ */
+/**
+ * Build ObjectExplorer Package
+ *
+ * Description: Build script for objectexplorer package
+ * @package objectexplorer
+ * @subpackage build
+ */
+
+/* Set package info be sure to set all of these */
+define('PKG_NAME','ObjectExplorer');
+define('PKG_NAME_LOWER','objectexplorer');
+define('PKG_VERSION','1.0.1');
+define('PKG_RELEASE','rc1');
+define('PKG_CATEGORY','ObjectExplorer');
+
+/* Set package options - you can turn these on one-by-one
+ * as you create the transport package
+ * */
+$hasAssets = true; /* Transfer the files in the assets dir. */
+$hasCore = true;   /* Transfer the files in the core dir. */
+$hasSnippets = true;
+$hasChunks = false;
+
+/******************************************
+ * Work begins here
+ * ****************************************/
+
+/* set start time */
+$mtime = microtime();
+$mtime = explode(" ", $mtime);
+$mtime = $mtime[1] + $mtime[0];
+$tstart = $mtime;
+set_time_limit(0);
+
+/* define sources */
+$root = dirname(dirname(__FILE__)) . '/';
+$sources= array (
+    'root' => $root,
+    'build' => $root . '_build/',
+    /* note that the next two must not have a trailing slash */
+    'source_core' => $root.'core/components/'.PKG_NAME_LOWER,
+    'source_assets' => $root.'assets/components/'.PKG_NAME_LOWER,
+    'resolvers' => $root . '_build/resolvers/',
+    'validators'=> $root . '_build/validators/',
+    'data' => $root . '_build/data/',
+    'docs' => $root . 'core/components/' . PKG_NAME_LOWER . '/docs/',
+    'install_options' => $root . '_build/install.options/',
+    'packages'=> $root . 'core/packages',
+);
+unset($root);
+
+/* Instantiate MODx -- if this require fails, check your
+ * _build/build.config.php file
+ */
+require_once $sources['build'].'build.config.php';
+require_once MODX_CORE_PATH . 'model/modx/modx.class.php';
+$modx= new modX();
+$modx->initialize('mgr');
+$modx->setLogLevel(xPDO::LOG_LEVEL_INFO);
+$modx->setLogTarget(XPDO_CLI_MODE ? 'ECHO' : 'HTML');
+
+/* load builder */
+$modx->loadClass('transport.modPackageBuilder','',false, true);
+$builder = new modPackageBuilder($modx);
+$builder->createPackage(PKG_NAME_LOWER, PKG_VERSION, PKG_RELEASE);
+$builder->registerNamespace(PKG_NAME_LOWER,false,true,'{core_path}components/'.PKG_NAME_LOWER.'/');
+
+
+/* create category  The category is required and will automatically
+ * have the name of your package
+ */
+
+$category= $modx->newObject('modCategory');
+$category->set('id',1);
+$category->set('category',PKG_CATEGORY);
+
+/* add snippets */
+if ($hasSnippets) {
+    $modx->log(modX::LOG_LEVEL_INFO,'Adding in snippets.');
+    $snippets = include $sources['data'].'transport.snippets.php';
+    /* note: Snippets' default properties are set in transport.snippets.php */
+    if (is_array($snippets)) {
+        $category->addMany($snippets, 'Snippets');
+    } else { $modx->log(modX::LOG_LEVEL_FATAL,'Adding snippets failed.'); }
+}
+
+if ($hasChunks) { /* add chunks  */
+    $modx->log(modX::LOG_LEVEL_INFO,'Adding in chunks.');
+    /* note: Chunks' default properties are set in transport.chunks.php */    
+    $chunks = include $sources['data'].'transport.chunks.php';
+    if (is_array($chunks)) {
+        $category->addMany($chunks, 'Chunks');
+    } else { $modx->log(modX::LOG_LEVEL_FATAL,'Adding chunks failed.'); }
+}
+
+
+/* Create Category attributes array dynamically
+ * based on which elements are present
+ */
+
+$attr = array(xPDOTransport::UNIQUE_KEY => 'category',
+    xPDOTransport::PRESERVE_KEYS => false,
+    xPDOTransport::UPDATE_OBJECT => true,
+    xPDOTransport::RELATED_OBJECTS => true,
+);
+
+
+if ($hasSnippets) {
+    $attr[xPDOTransport::RELATED_OBJECT_ATTRIBUTES]['Snippets'] = array(
+            xPDOTransport::PRESERVE_KEYS => false,
+            xPDOTransport::UPDATE_OBJECT => true,
+            xPDOTransport::UNIQUE_KEY => 'name',
+        );
+}
+
+if ($hasChunks) {
+    $attr[xPDOTransport::RELATED_OBJECT_ATTRIBUTES]['Chunks'] = array(
+            xPDOTransport::PRESERVE_KEYS => false,
+            xPDOTransport::UPDATE_OBJECT => true,
+            xPDOTransport::UNIQUE_KEY => 'name',
+        );
+}
+
+/* create a vehicle for the category and all the things
+ * we've added to it.
+ */
+$vehicle = $builder->createVehicle($category,$attr);
+
+
+
+/* This section transfers every file in the local
+ mycomponents/mycomponent/assets directory to the
+ target site's assets/mycomponent directory on install.
+ If the assets dir. has been renamed or moved, they will still
+ go to the right place.
+ */
+
+if ($hasCore) {
+    $vehicle->resolve('file',array(
+            'source' => $sources['source_core'],
+            'target' => "return MODX_CORE_PATH . 'components/';",
+        ));
+}
+
+/* This section transfers every file in the local 
+ mycomponents/mycomponent/core directory to the
+ target site's core/mycomponent directory on install.
+ If the core has been renamed or moved, they will still
+ go to the right place.
+ */
+
+    if ($hasAssets) {
+        $vehicle->resolve('file',array(
+            'source' => $sources['source_assets'],
+            'target' => "return MODX_ASSETS_PATH . 'components/';",
+        ));
+    }
+
+/* Put the category vehicle (with all the stuff we added to the
+ * category) into the package 
+ */
+$builder->putVehicle($vehicle);
+
+
+/* Next-to-last step - pack in the license file, readme.txt, changelog,
+ * and setup options 
+ */
+$builder->setPackageAttributes(array(
+    'license' => file_get_contents($sources['docs'] . 'license.txt'),
+    'readme' => file_get_contents($sources['docs'] . 'readme.txt'),
+    'changelog' => file_get_contents($sources['docs'] . 'changelog.txt'),
+));
+
+/* Last step - zip up the package */
+$builder->pack();
+
+/* report how long it took */
+$mtime= microtime();
+$mtime= explode(" ", $mtime);
+$mtime= $mtime[1] + $mtime[0];
+$tend= $mtime;
+$totalTime= ($tend - $tstart);
+$totalTime= sprintf("%2.4f s", $totalTime);
+
+$modx->log(xPDO::LOG_LEVEL_INFO, "Package Built.");
+$modx->log(xPDO::LOG_LEVEL_INFO, "Execution time: {$totalTime}");
+exit();
---
+++ _build/data/transport.snippets.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * ObjectExplorer transport snippets
+ * Copyright 2011 Your Name <you@yourdomain.com>
+ * @author Your Name <you@yourdomain.com>
+ * 1/1/11
+ *
+ * ObjectExplorer is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * ObjectExplorer is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ObjectExplorer; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package objectexplorer
+ */
+/**
+ * Description:  Array of snippet objects for ObjectExplorer package
+ * @package objectexplorer
+ * @subpackage build
+ */
+
+if (! function_exists('getSnippetContent')) {
+    function getSnippetContent($filename) {
+        $o = file_get_contents($filename);
+        $o = str_replace('<?php','',$o);
+        $o = str_replace('?>','',$o);
+        $o = trim($o);
+        return $o;
+    }
+}
+$snippets = array();
+
+$snippets[1]= $modx->newObject('modSnippet');
+$snippets[1]->fromArray(array(
+    'id' => 1,
+    'name' => 'ObjectExplorer',
+    'description' => 'ObjectExplorer Snippet.',
+    'snippet' => getSnippetContent($sources['source_core'].'/elements/snippets/objectexplorer.snippet.php'),
+),'',true,true);
+/*$properties = include $sources['data'].'/properties/properties.fileupload.php';
+$snippets[1]->setProperties($properties);
+unset($properties);*/
+
+return $snippets;
