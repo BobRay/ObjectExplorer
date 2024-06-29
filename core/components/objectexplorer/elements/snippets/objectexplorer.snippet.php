551fe8785c57e80263672be5eafb9283af98cd6c
bobray <bobray@softville.com>
2024-06-28 22:52:50 -0500

    Add license to snippet

--- core/components/objectexplorer/elements/snippets/objectexplorer.snippet.php
+++ core/components/objectexplorer/elements/snippets/objectexplorer.snippet.php
@@ -1,4 +1,26 @@
 <?php
+ /* ObjectExplorer snippet
+ *
+ * Copyright 2012-2024 Bob Ray <http:bobsguides.com>
+ *
+ * @author Bob Ray <https:bobsguides.com>
+ *
+ * ObjectExplorer is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * ObjectExplorer is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ObjectExplorer; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package ObjectExplorer
+ */
+
 /**
  * @package = ObjectExplorer
  *
@@ -10,7 +32,6 @@ $tstart = $mtime;
 set_time_limit(0);
 
 */
-
 
 if (!defined('MODX_CORE_PATH')) {
     $outsideModx = true;
